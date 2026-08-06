783af5d4f23fd0c06f83158c20495cd9fadda715
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-08-06 11:48:01 +0530

    client streaming rpc

--- client/main.go
+++ client/main.go
@@ -27,6 +27,8 @@ func main() {
 	}
 
 	// callSayHello(client)
-	callSayHelloServerStream(client, names)
+	// callSayHelloServerStream(client, names)
+	callSayHelloClientStream(client, names)
+	// callHelloBidirectionalStream(client, names)
 }
 
